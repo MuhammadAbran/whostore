8c1f78e89d50cbac3bf1da72d9ed083ebf867c9c
dayat fadila <dayat.recovery@gmail.com>
2019-01-28 21:04:07 +0700

    detail product

--- assets/screen/ProductDetail.js
+++ assets/screen/ProductDetail.js
@@ -1,16 +1,16 @@
 import React, {Component} from 'react';
 import {Image, View, ScrollView, FlatList, Alert, TouchableNativeFeedback} from 'react-native';
 import {
-   Container,
-   Content,
-   Header,
-   Card, Left, Right,
-   CardItem,
-   Body,
-   Title,
-   Text, Thumbnail,
-   Icon
-   } from 'native-base';
+ Container,
+ Content,
+ Header,
+ Card, Left, Right,
+ CardItem,
+ Body,
+ Title,
+ Text, Thumbnail,
+ Icon
+} from 'native-base';
 import { createStackNavigator, createAppContainer } from 'react-navigation';
 
 const color = '#3498db';
@@ -18,65 +18,37 @@ const numColumns = 2;
 
 
 export default class ProductDetail extends Component {
-   constructor(props){
-      super(props);
-      this.state ={
-         data:[]
-      }
-   }
+ constructor(props){
+  super(props);
+  this.state ={
+   data:[]
+ }
+}
 
-   fetchData = async()=> {
-      const responses = await fetch('http://sppd.dayatfadila.com/public/wootest/65');
-      const posts = await response.json();
-      // const pos = JSON.stringify(posts);
-      this.setState({data: posts});
-   }
+fetchData = async()=> {
+  const response = await fetch('http://sppd.dayatfadila.com/public/wootest/65');
+  const post = await response.json();
+      this.setState({data: post});
+    }
 
-   componentDidMount(){
+    componentDidMount(){
       this.fetchData();
-   }
-
-   static navigationOptions = {
-       title: 'Who Store?',
-       headerStyle: {
-         backgroundColor: color
-       },
-       headerTintColor: '#fff',
-     };
-
-     renderItem = ({ item, index }) => {
-        return (
-          <Card>
-              <CardItem cardBody>
-                 <TouchableNativeFeedback
-                    onPress={() => this.props.navigation.navigate('ProductDetail')}>
-                    <Image
-                       source={{ uri: item.images[0].src }}
-                       style={{ width:170, height: 170 }}
-                       />
-                 </TouchableNativeFeedback>
-              </CardItem>
-
-              <CardItem>
-                    <Text><Text style={{ fontWeight: 'bold' }}>{ item.name } </Text>{"\n"}Rp. { item.price }.000</Text>
-              </CardItem>
-          </Card>
-          );
     }
 
+    static navigationOptions = {
+     title: 'Who Store?',
+     headerStyle: {
+       backgroundColor: color
+     },
+     headerTintColor: '#fff',
+   };
 
-    render() {
+   render() {
     return (
-        <Container>
-          <ScrollView>
-              <FlatList
-                 data={this.state.data}
-                 renderItem={this.renderItem}
-                 numColumns={numColumns}
-                 keyExtractor={(item, index) => item.key}
-              />
-          </ScrollView>
-        </Container>
-    );
-    }
+    <Content>
+      <Text>{this.state.data.name}</Text>
+      </Content>
+      
+      );
+  }
 }
