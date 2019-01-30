797d5fe968ee2c4be541da6927b3dffa4c9598e6
MuhammadAbran <abran7815@gmail.com>
2019-01-30 19:14:35 +0700

    add to cart

--- assets/screen/ProductDetail.js
+++ assets/screen/ProductDetail.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {Image, View, ScrollView, FlatList, Alert, TouchableNativeFeedback} from 'react-native';
+import {Image, View, ScrollView, FlatList, Alert, TouchableNativeFeedback, TouchableOpacity, Button} from 'react-native';
 import Carousel from 'react-native-snap-carousel';
 import {
  Container,
@@ -16,7 +16,7 @@ import { createStackNavigator, createAppContainer } from 'react-navigation';
 
 const color = '#3498db';
 const numColumns = 2;
-
+const regex = /(<([^>]+)>)/;
 
 export default class ProductDetail extends Component {
  constructor(props){
@@ -30,50 +30,17 @@ export default class ProductDetail extends Component {
 fetchData = async()=> {
   const id = this.props.navigation.state.params.id;
   console.log(id);
-
   const response = await fetch('http://sppd.dayatfadila.com/public/wootest/'+id);
   const post = await response.json();
       this.setState({
          data: post
       });
 
-      // const images = JSON.stringify(this.state.data.images);
-      const images = [
-    {
-        title: 'Beautiful and dramatic Antelope Canyon',
-        subtitle: 'Lorem ipsum dolor sit amet et nuncat mergitur',
-        illustration: 'https://i.imgur.com/UYiroysl.jpg'
-    },
-    {
-        title: 'Earlier this morning, NYC',
-        subtitle: 'Lorem ipsum dolor sit amet',
-        illustration: 'https://i.imgur.com/UPrs1EWl.jpg'
-    },
-    {
-        title: 'White Pocket Sunset',
-        subtitle: 'Lorem ipsum dolor sit amet et nuncat ',
-        illustration: 'https://i.imgur.com/MABUbpDl.jpg'
-    },
-    {
-        title: 'Acrocorinth, Greece',
-        subtitle: 'Lorem ipsum dolor sit amet et nuncat mergitur',
-        illustration: 'https://i.imgur.com/KZsmUi2l.jpg'
-    },
-    {
-        title: 'The lone tree, majestic landscape of New Zealand',
-        subtitle: 'Lorem ipsum dolor sit amet',
-        illustration: 'https://i.imgur.com/2nCt3Sbl.jpg'
-    },
-    {
-        title: 'Middle Earth, Germany',
-        subtitle: 'Lorem ipsum dolor sit amet',
-        illustration: 'https://i.imgur.com/lceHsT6l.jpg'
-    }
-];
+  const images = this.state.data.images;
       this.setState({
          picture: images
       });
-      console.log(this.state.data);
+      console.log(this.state.data.description);
     }
 
     componentDidMount(){
@@ -88,20 +55,40 @@ fetchData = async()=> {
      headerTintColor: '#fff',
    };
 
-   _renderItem = (item, index) => {
+   _renderItem = ({ item, index }) => {
      return (
-      <View>
-                <Text>{ item.title }</Text>
-            </View>
+        <Image
+           source={{ uri: item.src }}
+           style={{ width: 380, height: 200 }}
+        />
      );
   }
 
    render() {
     return (
        <Content>
-       <ScrollView>
-          <Text>{this.state.data.name}</Text>
-          </ScrollView>
+          <Carousel
+              ref={(c) => { this._carousel = c; }}
+              data={this.state.picture}
+              renderItem={this._renderItem}
+              sliderWidth={380}
+              itemWidth={500}
+            />
+          <Content style={{ marginHorizontal: 12, marginTop: -25}}>
+             <Card>
+               <CardItem header>
+                   <Text style={{ fontSize: 20 }}>{this.state.data.name}</Text>
+               </CardItem>
+               <CardItem cardBody>
+                   <Text style={{ marginHorizontal: 10 }}>{this.state.data.description}</Text>
+               </CardItem>
+             </Card>
+             <Button
+                 onPress={ Alert.alert("yuhu~") }
+                 title="Add To Cart"
+               />
+          </Content>
+
        </Content>
 
       );
